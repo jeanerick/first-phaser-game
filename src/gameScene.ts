086192964457a87094598b2926edba95c520a6b4
Laboratório Informática <labinfo@unifil.br>
2019-04-23 22:11:25 -0300

    Capivara

--- src/gameScene.ts
+++ src/gameScene.ts
@@ -19,12 +19,13 @@ export class GameScene extends Phaser.Scene {
         this.delta = 1000;
         this.lastStarTime = 0;
         this.starsCaught = 0;
-        this.deaths = 0;
+        this.deaths = 3;
     }
+
     preload(): void {
         this.load.setBaseURL(
             "/");
-        this.load.image('player', 'assets/dude.jpg');
+        this.load.image('player', 'assets/capivara.jpg');
         this.load.image('fruta', 'assets/fruta.png');
     }
 
@@ -38,6 +39,8 @@ export class GameScene extends Phaser.Scene {
 
         this.player.setBounce(-1);
         this.player.setCollideWorldBounds(true);
+        this.player.setScale(.1);
+        this.player.flipX = true;
 
         this.input.keyboard.on('keydown-' + 'W', function (event) { instance.onKeyUp(event) });
         this.input.keyboard.on('keydown-' + 'S', function (event) { instance.onKeyDow(event) });
@@ -46,7 +49,6 @@ export class GameScene extends Phaser.Scene {
 
         for (let index = 0; index < this.maximumOfApples; index++) {
             this.createFruit();
-
         }
     }
 
@@ -58,11 +60,11 @@ export class GameScene extends Phaser.Scene {
                 this.delta -= 20;
             }
         }
-        this.info.text = `Placar : ${this.starsCaught} - Mortes : ${this.deaths} (max 3)`;
+        this.info.text = `Placar : ${this.starsCaught} - Vidas : ${this.deaths}`;
     }
 
     private onFail() {
-        if (this.deaths > 2) {
+        if (this.deaths == 0) {
             this.scene.start("ScoreScene",
                 { starsCaught: this.starsCaught });
         }
@@ -76,34 +78,38 @@ export class GameScene extends Phaser.Scene {
     }
 
     private onKeyUp(evnt) {
+        this.player.angle  = 90;
         this.player.setPosition(this.player.x, this.player.y - 10);
         //this.player.setAcceleration(0,-15)
         if (this.player.body.blocked.up) { this.killPlayer() }
     }
     private onKeyDow(evnt) {
+        this.player.angle  = -90;
         this.player.setPosition(this.player.x, this.player.y + 10);
         //this.player.setAcceleration(0, 15)
         if (this.player.body.blocked.down) { this.killPlayer() }
     }
     private onKeyLeft(evnt) {
+        this.player.flipX = false;
         this.player.setPosition(this.player.x - 10, this.player.y);
         //this.player.setAcceleration(-15,0)
         if (this.player.body.blocked.left) { this.killPlayer() }
     }
     private onKeyRight(evnt) {
+        this.player.flipX = true;
         this.player.setPosition(this.player.x + 10, this.player.y);
         //this.player.setAcceleration(15,0)
         if (this.player.body.blocked.right) { this.killPlayer() }
     }
 
     killPlayer(): void {
-        this.deaths += 1;
+        this.deaths -= 1;
         this.player.setTint(0xff0000)
         //this.player.setAcceleration(0,0)
         setTimeout(() => {
             this.player.clearTint();
             this.onFail();
-            this.player.setPosition(100, 100);
+            this.player.setPosition(100, 100);            
         }, 100);
 
     }
